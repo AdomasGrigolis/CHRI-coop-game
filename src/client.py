fixed force indicator, blackhole position + blackhole in server, haptic scaling

src/client.py
@@ -70,8 +70,9 @@ while running:
 
 # Haptic device initiate
 device_connected = False
+max_force = cfg_simulation["max_force"]
 G_ff = np.diag([1, 1])
-G_fb = np.diag([1/1000, 1/1000])
+G_fb = np.diag([1/max_force*5, 1/max_force*5])
 # Connect the device
 if player_number == 1:
     physics = Physics(hardware_version=2)
@@ -161,7 +162,7 @@ while run:
     
     # Haptic device force update
     if device_connected: #set forces only if the device is connected
-        physics.update_force(G_fb @ force_vector)
+        physics.update_force(G_fb @ -force_vector)
     
     # Read mouse position
     # Haptic device position
@@ -225,24 +226,25 @@ while run:
     
     # Add force meter
     force_meter_bg = pygame.Rect(10, 25, 200, 20)  # Background rectangle
-    force_meter_fill = pygame.Rect(10, 25, np.linalg.norm(force_vector) * 2, 20)  # Foreground rectangle that changes with force
-    print(force_vector)
-    pygame.draw.rect(window, (200, 200, 200), force_meter_bg)  # Draw background (light gray)
-    if 0<np.abs(np.linalg.norm(force_vector)) <25: 
-        pygame.draw.rect(window, (0, 255, 0), force_meter_fill)  # Draw fill (green)
-    elif 25<np.abs(np.linalg.norm(force_vector)) <50:
-        pygame.draw.rect(window, (255, 255, 0), force_meter_fill)  # Draw fill (YELLOW)
+    #force_meter_fill = pygame.Rect(10, 25, np.linalg.norm(force_vector) * 2, 20)  # Foreground rectangle that changes with force
 
-    elif 50 <= np.abs(np.linalg.norm(force_vector)):
+    # Force normalisation
+    # Normalize the force vector to fit within the meter
+    normalized_force = min(np.linalg.norm(force_vector) / max_force, 1.0)  # Clamp to [0, 1]
+    force_meter_fill_width = int(normalized_force * 200)  # Scale to meter width (200 pixels)
+
+    force_meter_fill = pygame.Rect(10, 25, force_meter_fill_width, 20)
+    max_force_line_x = 10 + 200 
+    pygame.draw.line(window, (255, 0, 0), (max_force_line_x, 25), (max_force_line_x, 45), 2)
+
+    if normalized_force < 0.25: 
+        pygame.draw.rect(window, (0, 255, 0), force_meter_fill)  # Draw fill (green)
+    elif 0.25 <= normalized_force < 0.5:
+        pygame.draw.rect(window, (255, 255, 0), force_meter_fill)  # Draw fill (yellow)
+    else:
         warning = pygame.transform.scale(warning, (50, 50))
         window.blit(warning, (pm[0], pm[1]))
-        pygame.draw.rect(window, (255, 0, 0), force_meter_fill)
-        #if high_force_start_time == 0:
-        #    high_force_start_time = pygame.time.get_ticks()
-
-        #current_time = pygame.time.get_ticks()
-        #if (current_time - high_force_start_time) / 1000 >= force_threshold_time:
-            #fail = True
+        pygame.draw.rect(window, (255, 0, 0), force_meter_fill)  # Draw fill (red)
     
     if timer != 3:
         countdown = TEXT_FONT.render(f'{timer}', True, (255, 255, 0))  
@@ -299,11 +301,15 @@ while run:
                     END_EFFECTOR_WIDTH, END_EFFECTOR_HEIGHT))
 
 
-    #add blackhole
+    # Draw blackhole
     blackhole = pygame.transform.scale(blackhole, (rad_obj*scale_factor, rad_obj*scale_factor))
+    # Calculate the top-left corner to center the black hole at (blackhole_x, blackhole_y)
+    blackhole_width, blackhole_height = blackhole.get_size()
+    top_left_x = blackhole_x - blackhole_width // 2
+    top_left_y = blackhole_y - blackhole_height // 2
 
-    # Generate random x and y coordinates within the window
-    window.blit(blackhole, (blackhole_x, blackhole_y))
+    # Draw the black hole
+    window.blit(blackhole, (top_left_x, top_left_y))
     
     if DEBUG: print('score:', score)
 

src/server.py
@@ -118,8 +118,21 @@ space.gravity = (0, int(100 * cfg_simulation['gravity']))
 init_object_pos = list(np.array(screen_size) * np.array(cfg_simulation['object']['init_position']))
 object_mass = cfg_simulation['object']['mass']
 
+# Collision filters
+WALL_CATEGORY = 0b001
+BALL_CATEGORY = 0b010
+ARM_CATEGORY = 0b100 
+LINK_CATEGORY = 0b1000
+LINK_MASK = 0b0000
+WALL_MASK = BALL_CATEGORY
+BALL_MASK = WALL_CATEGORY | ARM_CATEGORY
+ARM_MASK = BALL_CATEGORY
+
 ball = create_ball(space, init_object_pos, mass=1000, radius=random.randint(30, 100))
-floor = create_static_wall(space, (400, 580))
+floor = create_static_wall(space, (0, screen_size[1]), (screen_size[0], screen_size[1]), category=WALL_CATEGORY, mask=WALL_MASK)
+#left_wall = create_static_wall(space, (0, 0), (0, screen_size[1]), category=WALL_CATEGORY, mask=WALL_MASK)
+#right_wall = create_static_wall(space, (screen_size[0], 0), (screen_size[0], screen_size[1]), category=WALL_CATEGORY, mask=WALL_MASK)
+ceiling = create_static_wall(space, (0, 0), (screen_size[0], 0), category=WALL_CATEGORY, mask=WALL_MASK)
 arm1_link1, arm1_link2, end_effector_shape1 = create_arm(space, (xc-350, yc), 250, 200)
 arm2_link1, arm2_link2, end_effector_shape2 = create_arm(space, (xc+350, yc), 250, 200)
 #create_arm(space, (xc, yc), 150, 100)
@@ -129,7 +142,6 @@ arm2_link1, arm2_link2, end_effector_shape2 = create_arm(space, (xc+350, yc), 25
 # circle1_shape.color = (255, 0, 0, 255)
 # circle1_shape.filter = pymunk.ShapeFilter(group=1)  # Add this line
 end_effector_shape1.color = (255, 0, 0, 255)
-end_effector_shape1.filter = pymunk.ShapeFilter(group=1)  # Add this line
 mouse_body1 = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 mouse_joint1 = pymunk.PivotJoint(arm1_link2, mouse_body1, (200, 0), (0, 0))  # 100 是 arm2 的长度
 mouse_joint1.max_force = 100000  # the force of following
@@ -140,12 +152,20 @@ space.add(mouse_body1, mouse_joint1)
 # circle2_shape.color = (255, 255, 0, 0)
 # circle2_shape.filter = pymunk.ShapeFilter(group=2)  # Add this line
 end_effector_shape2.color = (255, 255, 0, 0)
-end_effector_shape2.filter = pymunk.ShapeFilter(group=2)  # Add this line
 mouse_body2 = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 mouse_joint2 = pymunk.PivotJoint(arm2_link2, mouse_body2, (200, 0), (0, 0))  # 100 是 arm2 的长度
 mouse_joint2.max_force = 100000  # the force of following
 mouse_joint2.error_bias = 0.01   # the smoothness of following
 space.add(mouse_body2, mouse_joint2)
+
+ball.filter = pymunk.ShapeFilter(categories=BALL_CATEGORY, mask=BALL_MASK)
+end_effector_shape1.filter = pymunk.ShapeFilter(categories=ARM_CATEGORY, mask=ARM_MASK)
+end_effector_shape2.filter = pymunk.ShapeFilter(categories=ARM_CATEGORY, mask=ARM_MASK)
+# Assign collision filters to the links
+arm1_link1.filter = pymunk.ShapeFilter(categories=LINK_CATEGORY, mask=LINK_MASK)
+arm1_link2.filter = pymunk.ShapeFilter(categories=LINK_CATEGORY, mask=LINK_MASK)
+arm2_link1.filter = pymunk.ShapeFilter(categories=LINK_CATEGORY, mask=LINK_MASK)
+arm2_link2.filter = pymunk.ShapeFilter(categories=LINK_CATEGORY, mask=LINK_MASK)
 
 
 ##ball.filter = pymunk.ShapeFilter(group=3)  # Assuming ball is the asteroid
@@ -179,12 +199,14 @@ latency_thread_instance.start()
 
 #initialize variables
 error_margin = cfg_simulation["error_margin"]
+max_force = cfg_simulation["max_force"]
+crush_force_factor = cfg_simulation["crush_force_factor"]
 fail = False
 success = False
 reset_required = False
 force_reset = False
 high_force_start_time = 0
-force_threshold_time = 5  # 5 seconds
+force_threshold_time = 1
 timer = 3 
 # MAIN LOOP
 trials = 1
@@ -224,8 +246,6 @@ while run:
             pm1 = pm
         elif addr == players[2]:  # Player 2
             pm2 = pm
-    # Print current position
-    #print(pm1, pm2)
 
     # save state
     current_state = [t, pm1, pm2, p1, p2, dp1, dp2, pr1, pr2, f1, f2, p_prev1, p_prev2]
@@ -350,7 +370,6 @@ while run:
         float(end_effector1_position_x), float(end_effector1_position_y),
         float(end_effector2_position_x), float(end_effector2_position_y)
     )
-
     # Send the serialized state to all players
     for player in players.values():
         try:
@@ -402,7 +421,7 @@ while run:
             last_timer_update = time.time()
 
     # Check if force exceeds threshold
-    if np.linalg.norm(f1) >= 50 or np.linalg.norm(f2) >= 50:
+    if np.linalg.norm(f1) > max_force*crush_force_factor or np.linalg.norm(f2) > max_force*crush_force_factor:
         # Start/continue counting time
         if high_force_start_time == 0:
             high_force_start_time = pygame.time.get_ticks()
@@ -420,12 +439,16 @@ while run:
     # real-time plotting
     window.fill((255,255,255)) # clear window
     
-    # print data
-    text = font.render("FPS = " + str( round( clock.get_fps() ) ), True, (0, 0, 0), (255, 255, 255))
-    window.blit(text, textRect)
-    
     space.step(1/FPS)
+
+    # Draw blackhole
+    pygame.draw.circle(window, (0, 0, 0), (blackhole_x, blackhole_y), radius=ball.radius)
     space.debug_draw(draw_options)
+
+    # print data
+    text = font.render("FPS = " + str( round( clock.get_fps() ) ), True, (0, 0, 0))
+    window.blit(text, textRect)
+
     pygame.display.flip() # update display
     
     # try to keep it real time with the desired step time